e0b2679937578a7cbe1802cff9b5e378e09edf35
unknown <brillosolutions@gmail.com>
2020-08-10 11:11:36 +0530

    v2.0.0

--- DS18B20.c
+++ DS18B20.c
@@ -2,8 +2,10 @@
 
 sbit sclPin at P3.B0; // I2C serial clock line
 sbit sdaPin at P3.B1; // I2C serial data line
-sbit DQ at P3.B2; // DS18B20 temprature sensor pin
+sbit DQ at P3.B2; // DS18B20 thermometer data line
+
 char backLight;
+char codeROM[8], mArr[9];
 
 void startI2C() // I2C start condition
 {
@@ -33,10 +35,10 @@ void writeToSlave(char mByte) // Writing byte to I2C slave
  }
 }
 
-void writeToI2C(char mAddr, char mByte)
+void writeToI2C(char codeROM, char mByte)
 {
   startI2C();
-  writeToSlave(mAddr);
+  writeToSlave(codeROM);
   writeToSlave(mByte);
   stopI2C();
 }
@@ -100,42 +102,35 @@ void setBackLight(int mBool)
     backLight = 0x00; // Turn OFF backlight of LCD
 }
 
-void delay(int mTime)
-{
-  int k;
-  for (k = 0; k < mTime; k++); // Delay in milliseconds
-}
- 
-char resetSensor()
+char resetMeter() // Reset thermometer and detect presense pulse
 {
   DQ = 0;
-  delay(29);
+  delay_us(480); // Reset pulse delay
   DQ = 1;
-  delay(28); // Wait for sensor to write 0 on DQ line
+  delay_us(112); // Wait for sensor to write presence (bit 0) on DQ line
+                 // Meter normally shows its presence within 60 - 240us (max 480us)
   return (DQ);
 }
 
 char getBit()
 {
   char k;
-  DQ = 0; // pull DQ low to start timeslot
-  DQ = 1; // then return high
-  for (k = 0; k < 3; k++); // delay 15us from start of timeslot
+  DQ = 0; // Pull DQ low to start timeslot
+  DQ = 1; // Then return high
+  delay_us(15); // Bit read timeslot
   return (DQ); // return value of DQ line
 }
 
 void setBit(char mByte)
 {
-  DQ = 0; // pull DQ low to start timeslot
-  
+  DQ = 0; // Pull DQ low to start timeslot
+
   if (mByte)
-    DQ = 1; // return DQ high if write 1
-  else
-    DQ = 0;
-    
-  delay(5); // hold value for remainder of timeslot
+    DQ = 1; // Return DQ high if write 1
+
+  delay_us(60); // Bit write timeslot
   DQ = 1;
-}// Delay provides 16us per loop, plus 24us. Therefore delay(5) = 104us
+}
 
 char getByte()
 {
@@ -143,9 +138,9 @@ char getByte()
     for (k = 0; k < 8; k++)
     {
       if(getBit())
-        mByte |= 0x01 << k; // reads byte in, one byte at a time and then shifts it left
-        
-      delay(6); // wait for rest of timeslot
+        mByte |= 0x01 << k; // Read one byte at a time then shift left
+
+      delay_us(45); // Bit read additional timeslot
     }
   return (mByte);
 }
@@ -153,63 +148,93 @@ char getByte()
 void putByte(char mByte)
 {
   char k, kt;
-    for (k = 0; k < 8; k++) // writes byte, one bit at a time
+    for (k = 0; k < 8; k++) // Write byte as one bit at a time
     {
-     kt = mByte >> k; // shifts val right 'i' spaces
-     kt &= 0x01; // copy that bit to temp
-     setBit(kt); // write bit in temp into
+      kt = mByte >> k; // Shift byte to right for k spaces
+      kt &= 0x01; // Copy that bit to temporary variable
+      setBit(kt); // Write temporary bit
     }
-  delay(5);
+}
+
+void matchROM()
+{
+  char k;
+  if (resetMeter() == 0)
+  {
+    putByte(0x55); // Match ROM, comapre 64-bit ROM code with meter's code
+    for (k = 0; k < 8; k++)
+      putByte(codeROM[k]);
+  }
+}
+
+void skipROM()
+{
+  if (resetMeter() == 0)
+    putByte(0xCC); // Skip ROM, dont read 64-bit ROM code in case of one meter
+}
+
+void readScratchPad()
+{
+  char k;
+  putByte(0xBE); // Read 9-byte scratchpad data including CRC
+  for (k = 0; k < 9; k++)
+    mArr[k] = getByte();
+}
+
+void convTemperature()
+{
+  putByte(0x44); // Start temperature conversion to be placed on scratchpad
 }
 
 char readTemperature()
 {
-  char mArr[9];
   char nByte, mByte;
-  char k, xByte, yByte;
-  
-  resetSensor();
-  putByte(0xCC); // Skip ROM
-  putByte(0x44); // Start termperature conversion
-  
-  delay(5);
-  
-  resetSensor();
-  putByte(0xCC); // Skip ROM
-  putByte(0xBE); // Read scratch pad
-  
-  for (k = 0; k < 9; k++)
-      mArr[k] = getByte();
 
-  nByte = mArr[0]; // Temperature LSB
-  mByte = mArr[1]; // Temperature MSB
-  k = nByte | mByte; // Merging MSB and LSB
-  xByte = (k & 0xF0) >> 4; // Correcting upper nibble of byte
-  yByte = (k & 0x0F) << 4; // Correcting lower nibble of byte
-  k = xByte | yByte; // Merging both nibbles
-  return (k);
+//  matchROM(); // Call this function only if multiple meters are on bus
+  skipROM(); // Call this function only if single meter is on bus
+  convTemperature();
+
+//  matchROM();
+  skipROM(); // Call this function only if single meter is on bus
+  readScratchPad();
+  
+  mByte = ((mArr[0] | mArr[1]) & 0xF0) >> 4; // Correcting upper nibble of byte
+  nByte = ((mArr[0] | mArr[1]) & 0x0F) << 4; // Correcting lower nibble of byte
+  return (mByte | nByte);
 }
 
-void main() 
+void readROM()
 {
-  char xByte, yByte;
+  char k;
+  if (resetMeter() == 0)
+  {
+    putByte(0x33); // Read ROM
+    for (k = 0; k < 8; k++)
+      codeROM[k] = getByte();
+  }
+}
+
+void main()
+{
+  char k, xByte, yByte;
   setBackLight(1); // Turning ON backlight
   initDisplay(); // Initializing LCD for 4-bit mode
+  readROM(); // Read 64-bit unique ROM code
   for (;;)
   {
-     xByte = yByte = readTemperature();
-     
-     xByte = (xByte / 10 * 16) + (xByte % 10); // Converting Hex. to Decimal
-     putChar(((xByte >> 4) | 0x30), 1, 7);
-     putChar(((xByte & 0x0F) | 0x30), 1, 8);
-     putChar(223, 1, 9); // Degree symbol decimal code is 223 and hex. is 0xDF
-     putChar('C', 1, 10);
-     
-     yByte = (yByte * 9 / 5) + 32; // Converting celcius to fahrenheit
-     yByte = (yByte / 10 * 16) + (yByte % 10);
-     putChar(((yByte >> 4) | 0x30), 2, 7);
-     putChar(((yByte & 0x0F) | 0x30), 2, 8);
-     putChar(223, 2, 9); // Degree symbol decimal code is 223 and hex. is 0xDF
-     putChar('F', 2, 10);
+    xByte = yByte = readTemperature();
+
+    xByte = (xByte / 10 * 16) + (xByte % 10); // Converting Hex. to Decimal
+    putChar(((xByte >> 4) | 0x30), 1, 7);
+    putChar(((xByte & 0x0F) | 0x30), 1, 8);
+    putChar(223, 1, 9); // Degree symbol decimal code is 223 and hex. is 0xDF
+    putChar('C', 1, 10);
+
+    yByte = (yByte * 9 / 5) + 32; // Converting celcius to fahrenheit
+    yByte = (yByte / 10 * 16) + (yByte % 10);
+    putChar(((yByte >> 4) | 0x30), 2, 7);
+    putChar(((yByte & 0x0F) | 0x30), 2, 8);
+    putChar(223, 2, 9); // Degree symbol decimal code is 223 and hex. is 0xDF
+    putChar('F', 2, 10);
   }
 }
